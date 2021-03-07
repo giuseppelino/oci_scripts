24de5d766339ca46e38730b888b2a19e545300a9
giuseppelino <36542317+giuseppelino@users.noreply.github.com>
2021-03-06 23:36:45 -0500

    Done

--- vm.tf
+++ vm.tf
@@ -1,22 +1,21 @@
-resource "oci_core_instance" "free_instance0" {
-  availability_domain = data.oci_identity_availability_domain.ad.name
-  compartment_id      = var.compartment_ocid
-  display_name        = "freeInstance0"
-  shape               = "VM.Standard.E2.1.Micro"
+resource "oci_core_instance" "ubuntu_instance" {
+    # Required
+    #availability_domain = "ocid1.compartment.oc1..aaaaaaaauh6e6z6lq7ynpuqiwmip42znv3unf2cqekkcpabslvy4bttvvxsq"
+    compartment_id = "ocid1.compartment.oc1..aaaaaaaauh6e6z6lq7ynpuqiwmip42znv3unf2cqekkcpabslvy4bttvvxsq"
+    shape = "VM.Standard.E2.1"
+    source_details {
+        source_id = "ocid1.image.oc1.iad.aaaaaaaak6gcgvlv3rgnrtztshfybjhbnarc5yakiunl2uu4bdadbmkqsf7q"
+        source_type = "image"
+    }
 
-  create_vnic_details {
-    subnet_id        = oci_core_subnet.test_subnet.id
-    display_name     = "primaryvnic"
-    assign_public_ip = true
-    hostname_label   = "freeinstance0"
-  }
-
-  source_details {
-    source_type = "image"
-    source_id   = var.images[var.region]
-  }
-
-  metadata = {
-    ssh_authorized_keys = var.ssh_public_key
-  }
+    # Optional
+    display_name = "<your-ubuntu-instance-name>"
+    create_vnic_details {
+        assign_public_ip = true
+        subnet_id = "ocid1.subnet.oc1.iad.aaaaaaaa6rghzgkecwfhiy2sfbvci37dmkyyt7e2hipxtpjbvpnuy2kakzwq"
+    }
+    metadata = {
+        ssh_authorized_keys = file("<ssh-public-key-path>")
+    }
+    preserve_boot_volume = false
 }
